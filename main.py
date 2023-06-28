882ff484c11c0cf95a3aa02998673c14840c32c5
Reis567 <matheusdosreis9@gmail.com>
2023-06-28 07:47:57 -0300

    Paddles and ball

---
+++ main.py
@@ -0,0 +1,40 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong by Reis567")
+wn.bgcolor("black")
+wn.setup(width=800,height=600)
+wn.tracer(0)
+
+# Raquete 1
+raquete_a = turtle.Turtle()
+raquete_a.speed(0)
+raquete_a.shape("square")
+raquete_a.color("white")
+raquete_a.shapesize(stretch_wid=5,stretch_len=1)
+raquete_a.penup()
+raquete_a.goto(-350,0)
+
+# Raquete 2
+raquete_b = turtle.Turtle()
+raquete_b.speed(0)
+raquete_b.shape("square")
+raquete_b.color("white")
+raquete_b.shapesize(stretch_wid=5,stretch_len=1)
+raquete_b.penup()
+raquete_b.goto(350,0)
+
+
+# BOLA
+
+bola = turtle.Turtle()
+bola.speed(0)
+bola.shape("square")
+bola.color("white")
+bola.penup()
+bola.goto(0,0)
+
+
+
+while True:
+    wn.update()
